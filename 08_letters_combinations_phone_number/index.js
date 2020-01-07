bec4d0d82c1b05ba9f4e71f8fbeede530cf43521
Fraser Tooth <frasertooth@gmail.com>
2020-01-07 18:10:15 +0900

    Buggy Base Written

--- 08_letters_combinations_phone_number/index.js
+++ 08_letters_combinations_phone_number/index.js
@@ -1,7 +1,7 @@
 // Given a string containing digits from 2-9 inclusive,
 // return all possible letter combinations that the number could represent.
 
-// A mapping of digit to letters (just like on the telephone buttons) is given below.
+// A mapping of digit to letters (just like on the telephone buttons) is given in the attached png
 // Note that 1 does not map to any letters.
 
 // Example:
@@ -17,6 +17,41 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {};
+var letterCombinations = function(digits) {
+  keymap = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+  };
+  const output = [];
+  const nums = digits.split("");
+
+  const findCombos = (numsLeft, stringSoFar = "") => {
+    //Base Case
+    if (numsLeft.length === 0) {
+      output.push(stringSoFar);
+      return;
+    }
+
+    //Iterable
+    numsLeft.forEach(num => {
+      if (keymap.hasOwnProperty(num)) {
+        const possibleLetters = keymap[num];
+        possibleLetters.forEach(letter => {
+          findCombos(numsLeft.splice(0, 1), stringSoFar + letter);
+        });
+      }
+    });
+  };
+
+  findCombos(nums);
+
+  return output;
+};
 
 module.exports = { letterCombinations };
--- 08_letters_combinations_phone_number/test.js
+++ 08_letters_combinations_phone_number/test.js
@@ -1,12 +1,57 @@
 //Imports
+const deepEqualInAnyOrder = require("deep-equal-in-any-order");
 const chai = require("chai");
+chai.use(deepEqualInAnyOrder);
 const { expect } = chai;
 const { letterCombinations } = require("./index");
 
 describe("Letter Combinations", () => {
-  it("Example Case", () => {
+  it.only("Example Case", () => {
     const input = "23";
     const expected = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
-    expect(letterCombinations(input)).to.deep.equal(expected);
+    expect(letterCombinations(input)).to.deep.equalInAnyOrder(expected);
+  });
+  it("Simple Case", () => {
+    const input = "9";
+    const expected = ["w", "x", "y", "z"];
+    expect(letterCombinations(input)).to.deep.equalInAnyOrder(expected);
+  });
+  it("Handle 1s Case", () => {
+    const input = "911";
+    const expected = ["w", "x", "y", "z"];
+    expect(letterCombinations(input)).to.deep.equalInAnyOrder(expected);
+  });
+  it("Repeated Case", () => {
+    const input = "222";
+    const expected = [
+      "aaa",
+      "aab",
+      "aac",
+      "aba",
+      "abb",
+      "abc",
+      "aca",
+      "acb",
+      "acc",
+      "baa",
+      "bab",
+      "bac",
+      "bba",
+      "bbb",
+      "bbc",
+      "bca",
+      "bcb",
+      "bcc",
+      "caa",
+      "cab",
+      "cac",
+      "cba",
+      "cbb",
+      "cbc",
+      "cca",
+      "ccb",
+      "ccc"
+    ];
+    expect(letterCombinations(input)).to.deep.equalInAnyOrder(expected);
   });
 });
