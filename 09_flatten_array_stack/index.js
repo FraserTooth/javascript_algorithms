54255a6f9b11aabf9a9d369af61797a01957df8f
Fraser Tooth <frasertooth@gmail.com>
2020-01-16 17:51:10 +0900

    Flatten Array with Stack Done

--- 09_flatten_array_stack/index.js
+++ 09_flatten_array_stack/index.js
@@ -5,6 +5,52 @@
  * @param {string} digits
  * @return {string[]}
  */
-const flatten = array => {};
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.items.length > 0) {
+      return this.items.pop();
+    }
+  }
+
+  peek() {
+    if (this.items.length > 0) {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
+
+const flatten = array => {
+  const stack = new Stack();
+  const output = [];
+
+  stack.push(array);
+
+  while (stack.size() > 0) {
+    const nextItem = stack.pop();
+    if (Array.isArray(nextItem)) {
+      //Fill Stack with Array
+      for (let i = 0; i < nextItem.length; i++) {
+        const item = nextItem[i];
+        stack.push(item);
+      }
+    } else {
+      output.push(nextItem);
+    }
+  }
+  return output;
+};
 
 module.exports = { flatten };
