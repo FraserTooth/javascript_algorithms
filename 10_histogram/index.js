d6cf1575725165d96551c3cba831c60682d26520
Fraser Tooth <frasertooth@gmail.com>
2020-01-17 13:57:14 +0900

    Histogram first pass

--- 10_histogram/index.js
+++ 10_histogram/index.js
@@ -1,10 +1,35 @@
 // Build a Histogram
+// Don't use a Nested Loop
 
 /**
  * @param {number[]} digits
  * @return {number[]}
  */
 
-const histogram = (data, buckets) => {};
+const histogram = (data, buckets, min, max) => {
+  const counts = {};
+  const output = Array(buckets).fill(0);
+  data.forEach(num => {
+    //Count Sort Bit
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  });
+
+  const bucketWidth = (max - min) / buckets;
+  for (num in counts) {
+    for (let i = 0; i < buckets; i++) {
+      const bucketBottom = i * bucketWidth + min;
+      const bucketTop = (i + 1) * bucketWidth + min;
+      if (num >= bucketBottom && num < bucketTop) {
+        output[i] += counts[num];
+        break;
+      }
+    }
+  }
+  return output;
+};
 
 module.exports = { histogram };
--- 10_histogram/test.js
+++ 10_histogram/test.js
@@ -9,19 +9,31 @@ describe("Histogram", () => {
   it("Histogram Simple Case 1", () => {
     const data = [1, 2, 3, 4];
     const buckets = 4;
+    const min = 0;
+    const max = 4.5;
     const expected = [1, 1, 1, 1];
-    expect(histogram(data, buckets)).to.deep.equalInAnyOrder(expected);
+    expect(histogram(data, buckets, min, max)).to.deep.equalInAnyOrder(
+      expected
+    );
   });
   it("Histogram Simple Case 2", () => {
     const data = [1, 2, 3, 4];
     const buckets = 2;
+    const min = 0;
+    const max = 4.5;
     const expected = [2, 2];
-    expect(histogram(data, buckets)).to.deep.equalInAnyOrder(expected);
+    expect(histogram(data, buckets, min, max)).to.deep.equalInAnyOrder(
+      expected
+    );
   });
   it("Histogram More Advanced Case", () => {
     const data = [0.5, 0.5, 0.3, -0.2, 1.6, 0, 0.1, 0.1, 0.6, 0.4];
     const buckets = 5;
+    const min = -0.5;
+    const max = 2;
     const expected = [1, 5, 3, 0, 1];
-    expect(histogram(data, buckets)).to.deep.equalInAnyOrder(expected);
+    expect(histogram(data, buckets, min, max)).to.deep.equalInAnyOrder(
+      expected
+    );
   });
 });
