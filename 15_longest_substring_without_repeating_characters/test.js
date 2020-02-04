84ae79276e5e0c35914bc5d5e4be62d53fe602c0
Fraser Tooth <frasertooth@gmail.com>
2020-02-04 17:31:06 +0900

    Set Up Longest Substring Task

---
+++ 15_longest_substring_without_repeating_characters/test.js
@@ -0,0 +1,51 @@
+//Imports
+const { expect } = require("chai");
+const { lengthOfLongestSubstring } = require("./index");
+
+describe("Longest Substring without Repeats", () => {
+  describe("Serialize", () => {
+    it("Base Case", () => {
+      const input = "abcabcbb";
+
+      const output = lengthOfLongestSubstring(input);
+
+      const expected = 3;
+
+      expect(output).to.deep.equal(expected);
+    });
+
+    it("Single Letter Case", () => {
+      const input = "bbbbbbbb";
+
+      const output = lengthOfLongestSubstring(input);
+
+      const expected = 1;
+
+      expect(output).to.deep.equal(expected);
+
+    });
+    it("Empty Case", () => {
+      const input = null;
+      const expected = 0;
+
+      const output = lengthOfLongestSubstring(input);
+
+      expect(output).to.deep.equal(expected);
+
+      const input2 = "";
+      const expected2 = 0;
+
+      const output2 = lengthOfLongestSubstring(input2);
+
+      expect(output2).to.deep.equal(expected2);
+    });
+    it("Example 2", () => {
+      const input = "pwwkew";
+      const expected = 3;
+
+      const output = lengthOfLongestSubstring(input);
+
+      expect(output).to.deep.equal(expected);
+    });
+  });
+});
