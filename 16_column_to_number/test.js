86dfc096e800bcdcd501d91279584cf6a1c63e70
Fraser Tooth <frasertooth@gmail.com>
2020-02-05 13:57:42 +0900

    Column to Number Added

---
+++ 16_column_to_number/test.js
@@ -0,0 +1,56 @@
+//Imports
+const { expect } = require("chai");
+const { titleToNumber } = require("./index");
+
+describe("Longest Substring without Repeats", () => {
+  describe("Serialize", () => {
+    it("Base Case", () => {
+      const input = "A";
+
+      const output = titleToNumber(input);
+
+      const expected = 1;
+
+      expect(output).to.deep.equal(expected);
+
+      const input2 = "Z";
+
+      const output2 = titleToNumber(input);
+
+      const expected2 = 26;
+
+      expect(output2).to.deep.equal(expected2);
+    });
+    it("Empty Case", () => {
+      const input = null;
+      const expected = 0;
+
+      const output = titleToNumber(input);
+
+      expect(output).to.deep.equal(expected);
+
+      const input2 = "";
+      const expected2 = 0;
+
+      const output2 = titleToNumber(input2);
+
+      expect(output2).to.deep.equal(expected2);
+    });
+    it("Example 2", () => {
+      const input = "AB";
+      const expected = 28;
+
+      const output = titleToNumber(input);
+
+      expect(output).to.deep.equal(expected);
+    });
+    it("Example 3", () => {
+      const input = "ZY";
+      const expected = 701;
+
+      const output = titleToNumber(input);
+
+      expect(output).to.deep.equal(expected);
+    });
+  });
+});
